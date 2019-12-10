972a7adc08352233f61ca46ff029cbf957d44e8a
Rafael Villalobos <rvilla3452@gmail.com>
2019-12-10 14:12:23 -0600

    Lista vista create con validaciones y seleccion de imagen sobresaliente. falta pulir controlador

--- WebApplication1/Controllers/UbicationsController.cs
+++ WebApplication1/Controllers/UbicationsController.cs
@@ -42,13 +42,9 @@ namespace WebApplication1.Controllers
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UbicationId,Name,DistritId")] Ubication ubication, string[] urls, int[] ubicationFeatures, string CantonId)
+        public ActionResult Create([Bind(Include = "UbicationId,Name,DistritId")] Ubication ubication, string[] urls, int[] ubicationFeatures, string CantonId, string outstandingPicture)
         {
             urls = urls != null ? urls : new string[0];
-
-
-
-
 
             if (ubication.Name != null && ubication.DistritId > 0 && urls.Length > 0 && ubicationFeatures.Length > 0)
             {
@@ -82,6 +78,13 @@ namespace WebApplication1.Controllers
                         db.UbicationFeaturesUbication.AddRange(Features);
                         db.SaveChanges();
 
+                        int a = 1;
+                        var i = 0;
+                        if (a > 0)
+                        {
+                            a /= i;
+                        }
+
                         transaction.Commit();
                         return RedirectToAction("Index");
 
@@ -89,7 +92,12 @@ namespace WebApplication1.Controllers
                     catch (Exception ex)
                     {
                         transaction.Rollback();
-                        ViewBag.Error = "ERROR" + ex.Message;
+                        reloadViewBags(urls, ubicationFeatures, outstandingPicture, CantonId);
+                        ViewBag.SelectedCanton = CantonId;
+                        ViewBag.SelectedDistrit = ubication.DistritId.ToString();
+                        return View(ubication);
+                       /* ViewBag.Error = "ERROR" + ex.Message;
+                        ViewBag.Selectedurl = Selectedurl;
                         ViewBag.urls = urls;
                         ViewBag.CantonId = new SelectList(db.Cantons.ToList(), "CantonId", "Name");
                         var distrits = from d in db.Distrits
@@ -100,22 +108,27 @@ namespace WebApplication1.Controllers
                         ViewBag.SelectedDistrit = ubication.DistritId.ToString();
                         ViewBag.UbicationFeaturesId = new SelectList(db.UbicationFeatures, "UbicationFeatureId", "Description");
                         ViewBag.SelectedUbicationFeatures = ubicationFeatures;
-                        return View(ubication);
+                        return View(ubication);*/
                     }
                 }
             }
+            return RedirectToAction("RenderUbicationsDataView", new { ubication, urls, ubicationFeatures, CantonId, outstandingPicture});
+        }
 
+
+        // Renderiza vista con datos ingresados x el usuario
+        private void reloadViewBags( string[] urls, int[] ubicationFeatures, string outstandingPicture, string CantonId)
+        {
+
+            ViewBag.Selectedurl = outstandingPicture;
             ViewBag.urls = urls;
+            var distrits = from d in db.Distrits
+                           where d.CantonId.ToString() == CantonId
+                           select d;
+            ViewBag.DistritId = new SelectList(distrits.ToList(), "DistritId", "Name");
             ViewBag.CantonId = new SelectList(db.Cantons.ToList(), "CantonId", "Name");
-            var distritList = from d in db.Distrits
-                              where d.CantonId.ToString() == CantonId
-                              select d;
-            ViewBag.DistritId = new SelectList(distritList.ToList(), "DistritId", "Name");
-            ViewBag.SelectedCanton = CantonId;
-            ViewBag.SelectedDistrit = ubication.DistritId.ToString();
             ViewBag.UbicationFeaturesId = new SelectList(db.UbicationFeatures, "UbicationFeatureId", "Description");
             ViewBag.SelectedUbicationFeatures = ubicationFeatures;
-            return View(ubication);
         }
 
         // GET: Ubications/Edit/5
