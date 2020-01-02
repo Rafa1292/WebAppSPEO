4bcdcd84bf6d06d78e9c9a7a51d394106030091a
Rafael Villalobos <rvilla3452@gmail.com>
2020-01-02 09:31:20 -0600

    Previo cambios ViewModel

---
+++ WebApplication1/ViewModel/ArticleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModel
+{
+    public class ArticleViewModel
+    {
+        public Article Article { get; set; }
+        public Terrain Terrain { get; set; }
+        public House House { get; set; }
+        public HouseAux HouseAux { get; set; }
+        public string[] Urls { get; set; }
+        public int[] HouseFeatures { get; set; }
+        public int[] HouseFeturesAux { get; set; }
+        public int[] TerrainFeatures { get; set; }
+        public string outstandingPicture { get; set; }
+
+    }
+}
