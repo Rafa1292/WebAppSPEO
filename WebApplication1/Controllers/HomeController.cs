6692296ff0b2611a1d8aacdcbb9fe5cb463e2701
Rafael Villalobos <rvilla3452@gmail.com>
2020-01-25 15:33:12 -0600

    Previo agregar campo description a ubications

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,28 +3,175 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.ViewModel;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private WebApplication1Context db = new WebApplication1Context();
+
         public ActionResult Index()
         {
-            return View();
+            var articlesEF = from a in db.Articles
+                             where a.State && !a.SoldState
+                             select a;
+
+            var articles = articlesEF.ToList();
+
+
+            List<ArticleViewModel> ArticleViewModelList = new List<ArticleViewModel>();
+            foreach (var article in articles)
+            {
+                ArticleViewModel articleViewModel = GetArticleViewModel(article);
+                ArticleViewModelList.Add(articleViewModel);
+            }
+
+            ViewBag.Ubications = db.Ubications.ToList();
+            return View(ArticleViewModelList);
         }
 
-        public ActionResult About()
+        public ArticleViewModel GetArticleViewModel(Article article)
         {
-            ViewBag.Message = "Your application description page.";
+            var pictures = GetPictures(article.Id);
+            string[] urls = pictures.Urls;
+            string outstandingPicture = pictures.OutstandingPicture;
+            var houses = GetHouses(article.Id);
+            House house = houses.House != null ? houses.House : null;
+            HouseAux houseAux = houses.HouseAux != null ? houses.HouseAux : null;
+            ArticleViewModel Features = GetFeatures(article, house, houseAux);
+            int[] terrainFeatures = Features.TerrainFeatures;
+            int[] houseFeatures = Features.HouseFeatures;
+            int[] houseAuxFeatures = Features.HouseAuxFeatures;
+            ArticleViewModel articleViewModel = SetArticleViewModel(article, urls, outstandingPicture, house, houseAux, houseFeatures, houseAuxFeatures, terrainFeatures);
 
-            return View();
+            return articleViewModel;
         }
 
-        public ActionResult Contact()
+        public ArticleViewModel SetArticleViewModel(
+    Article article, string[] urls, string outstandingPicture, House house, HouseAux houseAux, int[] houseFeatures, int[] houseAuxFeatures, int[] terrainFeatures)
         {
-            ViewBag.Message = "Your contact page.";
+            ArticleViewModel articleViewModel = new ArticleViewModel();
+            articleViewModel.Article = article;
+            articleViewModel.Currency = article.Currency;
+            articleViewModel.Description = article.Description;
+            articleViewModel.Terrain = article.Terrain;
+            articleViewModel.UbicationId = article.UbicationId;
+            articleViewModel.IndividualContributorId = article.IndividualContributorId;
+            articleViewModel.Urls = urls;
+            articleViewModel.OutstandingPicture = outstandingPicture;
+            articleViewModel.House = house;
+            articleViewModel.HouseAux = houseAux;
+            articleViewModel.HouseFeatures = houseFeatures;
+            articleViewModel.HouseAuxFeatures = houseAuxFeatures;
+            articleViewModel.TerrainFeatures = terrainFeatures;
+            return articleViewModel;
+        }
 
-            return View();
+        public ArticleViewModel GetPictures(int id)
+        {
+            ArticleViewModel articleViewModel = new ArticleViewModel();
+            List<ArticlePicture> Pictures = db.ArticlePictures.ToList();
+            IEnumerable<string> urls = from p in Pictures
+                                       where p.ArticleId == id && p.OutstandingPicture == false
+                                       select Convert.ToBase64String(p.PictureArray);
+
+            var outstandingPictureModel = Pictures.Find(x => x.OutstandingPicture == true && x.ArticleId == id);
+            string outstandingPicture = Convert.ToBase64String(outstandingPictureModel.PictureArray);
+
+
+            articleViewModel.Urls = urls.ToArray();
+            articleViewModel.OutstandingPicture = outstandingPicture;
+
+            return articleViewModel;
+        }
+
+        public ArticleViewModel GetFeatures(Article article, House house, HouseAux houseAux)
+        {
+            ArticleViewModel articleViewModel = new ArticleViewModel();
+
+            var terrainFeaturesid = from f in db.TerrainFeaturesTerrain
+                                    where f.TerrainId == article.TerrainId
+                                    select f.TerrainFeatureId;
+            ICollection<int> terrainFeatures = new List<int>();
+            foreach (var feature in terrainFeaturesid)
+            {
+                terrainFeatures.Add(feature);
+            }
+
+            articleViewModel.TerrainFeatures = terrainFeatures.ToArray();
+
+
+            var houseFeaturesid = from h in db.HouseFeatureHouse
+                                  where h.HouseId == house.HouseId
+                                  select h.HouseFeatureId;
+
+            if (house != null)
+            {
+                ICollection<int> houseFeatures = new List<int>();
+                foreach (var feature in houseFeaturesid)
+                {
+                    houseFeatures.Add(feature);
+                }
+                articleViewModel.HouseFeatures = houseFeatures.ToArray();
+            }
+
+            var houseAuxFeaturesid = from h in db.HouseFeatureHouse
+                                     where h.HouseId == houseAux.Id
+                                     select h.HouseFeatureId;
+
+            if (houseAux != null)
+            {
+                ICollection<int> houseAuxFeatures = new List<int>();
+                foreach (var feature in houseAuxFeaturesid)
+                {
+                    houseAuxFeatures.Add(feature);
+                }
+                articleViewModel.HouseAuxFeatures = houseAuxFeatures.ToArray();
+
+            }
+
+
+
+            return articleViewModel;
+        }
+
+        public ArticleViewModel GetHouses(int id)
+        {
+            ArticleViewModel articleViewModel = new ArticleViewModel();
+
+            IQueryable<House> HouseList = from h in db.Houses
+                                          where h.ArticleId == id
+                                          select h;
+
+            House house = null;
+            HouseAux houseAux = null;
+
+            foreach (var houseResult in HouseList)
+            {
+                if (house == null)
+                {
+                    house = houseResult;
+                }
+                else
+                {
+                    houseAux = new HouseAux();
+                    houseAux.Id = houseResult.HouseId;
+                    houseAux.BathroomsAux = houseResult.Bathrooms;
+                    houseAux.BedroomsAux = houseResult.Bedrooms;
+                    houseAux.GarageAux = houseResult.Garage;
+                    houseAux.HouseBackgroundMeasureAux = houseResult.HouseBackgroundMeasure;
+                    houseAux.HouseForeheadMeasureAux = houseResult.HouseForeheadMeasure;
+                    houseAux.LevelsAux = houseResult.Levels;
+
+                }
+            }
+
+            articleViewModel.House = house;
+            articleViewModel.HouseAux = houseAux;
+
+            return articleViewModel;
         }
     }
 }
